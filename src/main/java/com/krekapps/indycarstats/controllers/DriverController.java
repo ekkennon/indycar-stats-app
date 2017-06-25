cc3791539ac325a40c3a7f2f7a31b60c31a3c243
Erin K. Kennon <ekkennon@gmail.com>
2017-06-25 09:39:01 -0500

    minor path fixes

--- src/main/java/com/krekapps/indycarstats/controllers/DriverController.java
+++ src/main/java/com/krekapps/indycarstats/controllers/DriverController.java
@@ -1,7 +1,40 @@
 package com.krekapps.indycarstats.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by ekk on 25-Jun-17.
  */
+
+@Controller
+@RequestMapping(value="drivers")
 public class DriverController {
+
+    @RequestMapping(value="")
+    private String index(Model model) {
+        model.addAttribute("title", "IndyCar Drivers");
+        return "drivers/index";
+    }
+
+    @RequestMapping(value="view")
+    private String viewAll(Model model) {
+        model.addAttribute("title", "IndyCar Drivers List");
+        return "drivers/view";
+    }
+
+    @RequestMapping(value="view/{id}")
+    private String viewOne(Model model, @PathVariable int id) {
+        model.addAttribute("title", "IndyCar Driver: ");
+        return "drivers/view";
+    }
+
+    @RequestMapping(value="add", method= RequestMethod.GET)
+    private String add(Model model) {
+        model.addAttribute("title", "Add IndyCar Driver");
+        return "drivers/add";
+    }
 }
--- src/main/java/com/krekapps/indycarstats/models/Driver.java
+++ src/main/java/com/krekapps/indycarstats/models/Driver.java
@@ -1,7 +1,17 @@
 package com.krekapps.indycarstats.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by ekk on 25-Jun-17.
  */
+
+@Entity
 public class Driver {
+
+    @Id
+    @GeneratedValue
+    private int id;
 }
