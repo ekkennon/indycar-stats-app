537034175228dd4e4b82c57074382304ea07fb90
Erin K. Kennon <ekkennon@gmail.com>
2017-06-25 20:57:51 -0500

    added view pages

--- src/main/java/com/krekapps/indycarstats/controllers/ListController.java
+++ src/main/java/com/krekapps/indycarstats/controllers/ListController.java
@@ -1,9 +1,7 @@
 package com.krekapps.indycarstats.controllers;
 
-import com.krekapps.indycarstats.models.Driver;
-import com.krekapps.indycarstats.models.Track;
-import com.krekapps.indycarstats.models.data.DriverDao;
-import com.krekapps.indycarstats.models.data.TrackDao;
+import com.krekapps.indycarstats.models.*;
+import com.krekapps.indycarstats.models.data.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,10 +20,34 @@ import java.util.List;
 
 @Controller
 public class ListController {
-    private String viewListTitle = "IndyCar Drivers List";
+    private String statsTitle = "IndyCar Stats List";
 
     @Autowired
     private DriverDao driverDao;
+
+    @Autowired
+    private RaceDao raceDao;
+
+    @Autowired
+    private SeasonDao seasonDao;
+
+    @Autowired
+    private SessionDao sessionDao;
+
+    @Autowired
+    private DecimalStatDao decimalStatDao;
+
+    @Autowired
+    private IntStatDao intStatDao;
+
+    @Autowired
+    private StringStatDao stringStatDao;
+
+    @Autowired
+    private TimeStatDao timeStatDao;
+
+    @Autowired
+    private TeamDao teamDao;
 
     @Autowired
     private TrackDao trackDao;
@@ -46,6 +68,150 @@ public class ListController {
         model.addAttribute("title", "IndyCar Driver: " + driver.getName());
         model.addAttribute("drivers", d);
         return "drivers/view";
+    }
+
+    @RequestMapping(value="races/view")
+    private String racesViewAll(Model model) {
+        model.addAttribute("title", "IndyCar Races List");
+        model.addAttribute("races", raceDao.findAll());
+        return "races/view";
+    }
+
+    @RequestMapping(value="races/view/{id}")
+    private String racesViewOne(Model model, @PathVariable int id) {
+        Race race = raceDao.findOne(id);
+        List<Race> racelist = new ArrayList<>();
+        racelist.add(race);
+        Iterable<Race> r = racelist;
+        model.addAttribute("title", "IndyCar Race: " + race.toString());
+        model.addAttribute("races", r);
+        return "races/view";
+    }
+
+    @RequestMapping(value="seasons/view")
+    private String seasonsViewAll(Model model) {
+        model.addAttribute("title", "IndyCar Seasons List");
+        model.addAttribute("seasons", seasonDao.findAll());
+        return "seasons/view";
+    }
+
+    @RequestMapping(value="seasons/view/{id}")
+    private String seasonsViewOne(Model model, @PathVariable int id) {
+        Season season = seasonDao.findOne(id);
+        List<Season> seasonlist = new ArrayList<>();
+        seasonlist.add(season);
+        Iterable<Season> s = seasonlist;
+        model.addAttribute("title", "IndyCar Season: " + season.getYear());
+        model.addAttribute("seasons", s);
+        return "seasons/view";
+    }
+
+    @RequestMapping(value="sessions/view")
+    private String sessionsViewAll(Model model) {
+        model.addAttribute("title", "IndyCar Sessions List");
+        model.addAttribute("sessions", sessionDao.findAll());
+        return "sessions/view";
+    }
+
+    @RequestMapping(value="sessions/view/{id}")
+    private String sessionsViewOne(Model model, @PathVariable int id) {
+        Session session = sessionDao.findOne(id);
+        List<Session> sessionlist = new ArrayList<>();
+        sessionlist.add(session);
+        Iterable<Session> s = sessionlist;
+        model.addAttribute("title", "IndyCar Session: " + session.getName());
+        model.addAttribute("sessions", s);
+        return "sessions/view";
+    }
+
+    @RequestMapping(value="stats/decimal/view")
+    private String statsDecimalViewAll(Model model) {
+        model.addAttribute("title", statsTitle);
+        model.addAttribute("stats", decimalStatDao.findAll());
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/int/view")
+    private String statsIntViewAll(Model model) {
+        model.addAttribute("title", statsTitle);
+        model.addAttribute("stats", intStatDao.findAll());
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/string/view")
+    private String statsDtringViewAll(Model model) {
+        model.addAttribute("title", statsTitle);
+        model.addAttribute("stats", stringStatDao.findAll());
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/time/view")
+    private String statsTimeViewAll(Model model) {
+        model.addAttribute("title", statsTitle);
+        model.addAttribute("stats", timeStatDao.findAll());
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/decimal/view/{id}")
+    private String statsDecimalViewOne(Model model, @PathVariable int id) {
+        DecimalStat stat = decimalStatDao.findOne(id);
+        List<DecimalStat> statlist = new ArrayList<>();
+        statlist.add(stat);
+        Iterable<DecimalStat> s = statlist;
+        model.addAttribute("title", "IndyCar Stat: " + stat.getName());
+        model.addAttribute("stats", s);
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/int/view/{id}")
+    private String statsIntViewOne(Model model, @PathVariable int id) {
+        IntStat stat = intStatDao.findOne(id);
+        List<IntStat> statlist = new ArrayList<>();
+        statlist.add(stat);
+        Iterable<IntStat> s = statlist;
+        model.addAttribute("title", "IndyCar Stat: " + stat.getName());
+        model.addAttribute("stats", s);
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/string/view/{id}")
+    private String statsStringViewOne(Model model, @PathVariable int id) {
+        StringStat stat = stringStatDao.findOne(id);
+        List<StringStat> statlist = new ArrayList<>();
+        statlist.add(stat);
+        Iterable<StringStat> s = statlist;
+        model.addAttribute("title", "IndyCar Stat: " + stat.getName());
+        model.addAttribute("stats", s);
+        return "stats/view";
+    }
+
+    @RequestMapping(value="stats/time/view/{id}")
+    private String statsTimeViewOne(Model model, @PathVariable int id) {
+        TimeStat stat = timeStatDao.findOne(id);
+        List<TimeStat> statlist = new ArrayList<>();
+        statlist.add(stat);
+        Iterable<TimeStat> s = statlist;
+        model.addAttribute("title", "IndyCar Stat: " + stat.getName());
+        model.addAttribute("stats", s);
+        return "stats/view";
+    }
+
+    @RequestMapping(value="teams/view")
+    private String teamsViewAll(Model model) {
+        model.addAttribute("title", "IndyCar Teams List");
+        model.addAttribute("teams", teamDao.findAll());
+        return "teams/view";
+    }
+
+    @RequestMapping(value="teams/view/{id}")
+    private String teamsViewOne(Model model, @PathVariable int id) {
+        Team team = teamDao.findOne(id);
+        List<Team> teamlist = new ArrayList<>();
+        teamlist.add(team);
+        Iterable<Team> t = teamlist;
+        model.addAttribute("title", "IndyCar Team: " + team.getName());
+        model.addAttribute("teams", t);
+        return "teams/view";
     }
 
     @RequestMapping(value="tracks/view")
